51d9722332a7be35e088cfbcaf4a11f093167d78
Leonardo Chonkan <lchonkan@forgebim.com>
2020-04-21 12:13:18 -0600

    adds functionality to fecth the artwork from the speakerServices and provide it to the templates in hte speakers router

--- routes/speakers.js
+++ routes/speakers.js
@@ -12,13 +12,25 @@ module.exports = (params) => {
 
   router.get('/', async (request, response) => {
     const speakers = await speakerService.getList();
-    response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
+    const allArtwork = await speakerService.getAllArtwork();
+    response.render('layout', {
+      pageTitle: 'Speakers',
+      template: 'speakers',
+      speakers,
+      allArtwork,
+    });
   });
 
   router.get('/:shortname', async (request, response) => {
     const speaker = await speakerService.getSpeaker(request.params.shortname);
+    const speakerArtwork = await speakerService.getArtworkForSpeaker();
     console.log(speaker);
-    response.render('layout', { pageTitle: 'Speakers', template: 'speakers-detail', speaker });
+    response.render('layout', {
+      pageTitle: 'Speakers',
+      template: 'speakers-detail',
+      speaker,
+      speakerArtwork,
+    });
   });
 
   return router;
