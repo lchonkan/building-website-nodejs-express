c035839758b6a6853c106a4c82a31f554c9fd067
Leonardo Chonkan <lchonkan@forgebim.com>
2020-04-21 22:54:25 -0600

    Adds functionality to handle the data in the form and store it in the feedback.json file

--- routes/feedback.js
+++ routes/feedback.js
@@ -13,22 +13,26 @@ module.exports = (params) => {
     try {
       const feedback = await feedbackService.getList();
 
-      //*
+      // $ here we evaluate if the there are any feedback ERRORS coming from calling
+      // $ the GET method from the error function in the POST method.
       const errors = request.session.feedback ? request.session.feedback.errors : false;
       request.session.feedback = {};
+
+      const successMessage = request.session.feedback ? request.session.feedback.message : false;
 
       return response.render('layout', {
         pageTitle: 'Feedback',
         template: 'feedback',
         feedback,
         errors,
+        successMessage,
       });
     } catch (err) {
       return next(err);
     }
   });
 
-  //$ The router on the feedbackjs file now contains an express-validator to check the inputs
+  // $ The router on the feedbackjs file now contains an express-validator to check the inputs
   router.post(
     '/',
     [
@@ -37,7 +41,8 @@ module.exports = (params) => {
       check('title').trim().isLength({ min: 3 }).escape().withMessage('A title is required'),
       check('message').trim().isLength({ min: 3 }).escape().withMessage('A message is required'),
     ],
-    (request, response) => {
+
+    async (request, response) => {
       // Here we add the errors var with the validationResult method from the express-validator
       const errors = validationResult(request);
 
@@ -52,8 +57,17 @@ module.exports = (params) => {
         // this redirect response is because everytime you send a form you need to avoid that
         // the user can hit the reload button to send it again.
       }
-      //console.log(request.body); // we are getting this body from the body-parser middleware in the server.js file
-      return response.send(`Feedback from posted`);
+      // console.log(request.body); // we are getting this body from the body-parser middleware in the server.js file
+
+      // $ Here we fetch the arguments of the add entry function from the request.body object that is send by the POST method.
+      const { name, email, title, message } = request.body;
+      await feedbackService.addEntry(name, email, title, message);
+      request.session.feedback = {
+        message: 'Thank you for your feedback!',
+      };
+      // $ Redirect to the
+      return response.redirect(`/Feedback`);
+      // Redirect to the GET ROUTE
     }
   );
 
